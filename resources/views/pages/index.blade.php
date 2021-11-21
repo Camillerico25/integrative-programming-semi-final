4072d551b2ce0dee495b884cc9a9582ffac15f74
unknown <chadiegilaugis@gmail.com>
2021-11-21 09:06:50 +0800

    ipt-semi

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container text-center mt-5  ">
+    <h1>Home Page</h1>
+
+    <div class="container mt-5">
+        <h2>Camille Rico BSIT-3</h2>
+    </div>
+</div>
+@endsection
